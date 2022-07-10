bd1e18b37c062a010c4fc3a31c55683891eac652
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 00:43:54 +0300

    Pataisyta funkcija pagal naujai suprasta salyga. Reikia ne kiek unikaliu, o tiesiog visas skirtingasd suskaiciuot neiskaiciuojant duplikatu. Pataisyta su HashSetu, bet senas pavyzdys taip pat paliktas kaip kad papildomas jei salyga butu buvus truputi kitokia

--- src/BoyOrGirl.java
+++ src/BoyOrGirl.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 // https://codeforces.com/problemset/problem/236/A
@@ -5,25 +6,43 @@ public class BoyOrGirl {
     public static void main(String[] args) {
         Scanner skait = new Scanner(System.in);
         String username = skait.nextLine();
-        System.out.println(" = " + kiekYraUnikaliuRaidziuZodyje(username));
         rezultatoIsvedimas(username);
     }
 
+//    /**
+//     * Suranda kiek unikalių raidžių yra žodyje. Veikia tokiu principu:
+//     * Eina per kiekvieną raidę ir tikrina ar tos raidės pirmo pasikartojimo (indexOf()) indeksas sutampa su paskutiniu pasikartojimu (lastIndexOf()), jeigu sutampa - ta raidė unikali.
+//     * Pvz. žodyje tvartas: raidės 't' indexOf() bus 0, o lastIndexOf() bus 4 - jie nesutampa, reiškias ir t raidė nebus unikali. O pvz. v raidės indexOf() bus 1 bei tu pačiu ir lastIndexOf() bus 1, todėl ta raidė yra unikali
+//     * @param zodis
+//     * @return
+//     */
+//    public static int kiekYraUnikaliuRaidziuZodyje(String zodis) {
+//        int kiekis = 0;
+//        for (int i = 0; i < zodis.length(); i++) {
+//            if (zodis.indexOf(zodis.charAt(i)) == zodis.lastIndexOf(zodis.charAt(i))) {
+//                kiekis++;
+//            }
+//            else {
+//                System.out.println("zodis.charAt(i) = " + zodis.charAt(i));
+//                System.out.println("zodis.indexOf(zodis.charAt(i)) = " + zodis.indexOf(zodis.charAt(i)));
+//                System.out.println("zodis.lastIndexOf(zodis.charAt(i)) = " + zodis.lastIndexOf(zodis.charAt(i)));
+//            }
+//        }
+//        return kiekis;
+//    }
+
     /**
-     * Suranda kiek unikalių raidžių yra žodyje. Veikia tokiu principu:
-     * Eina per kiekvieną raidę ir tikrina ar tos raidės pirmo pasikartojimo (indexOf()) indeksas sutampa su paskutiniu pasikartojimu (lastIndexOf()), jeigu sutampa - ta raidė unikali.
-     * Pvz. žodyje tvartas: raidės 't' indexOf() bus 0, o lastIndexOf() bus 4 - jie nesutampa, reiškias ir t raidė nebus unikali. O pvz. v raidės indexOf() bus 1 bei tu pačiu ir lastIndexOf() bus 1, todėl ta raidė yra unikali
-     * @param zodis
-     * @return
+     * Pasirodo čia reikėjo rasti ne kiek yra unikalių, bet tiesiog dubliuojančių raidžių neįskaityti. Pvz žodyje "wjmzbmr" yra 6 skirtingos raidės. Nes nors ir `m` raidė kartojassi, ją vis vien VIENĄ kartą įskaičiuoja.
+     * Kadangi sąlyga truputį "pasikeitė" (yra padarytas kitam variantui metodas) tai šiam variantui geriausiai tiktų HashSet naudojimas. Set - tai duomenų struktūra, kurioje negali būti duplikatų
+     * @param zodis tikrinamas žodis
+     * @return kiek yra raidžių skirtingų
      */
     public static int kiekYraUnikaliuRaidziuZodyje(String zodis) {
-        int kiekis = 0;
+        HashSet<Character> skirtingosRaides = new HashSet<>();
         for (int i = 0; i < zodis.length(); i++) {
-            if (zodis.indexOf(zodis.charAt(i)) == zodis.lastIndexOf(zodis.charAt(i))) {
-                kiekis++;
-            }
+            skirtingosRaides.add(zodis.charAt(i));
         }
-        return kiekis;
+        return skirtingosRaides.size();
     }
 
     /**
@@ -41,7 +60,7 @@ public class BoyOrGirl {
      * @param zodis žodis
      */
     public static void rezultatoIsvedimas(String zodis) {
-        if (arLyginis(kiekYraUnikaliuRaidziuZodyje(zodis))) {
+        if (!arLyginis(kiekYraUnikaliuRaidziuZodyje(zodis))) {
             System.out.println("IGNORE HIM!");
         }
         else {
