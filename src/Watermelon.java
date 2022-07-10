fb7c00634691d58c7316337e7ce9c4ee49317023
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-11 00:05:51 +0300

    Rezultato gavimo funkcijos

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 // https://codeforces.com/problemset/problem/4/A
 public class Watermelon {
     public static void main(String[] args) {
+        Scanner skaitytuvas = new Scanner(System.in);
+        int w = skaitytuvas.nextInt();          // arbūzo svoris
 
+        System.out.println(konvertuojaYesIrNo(arSkaiciusLyginisIrNeDvejetas(w)));
+
+    }
+
+    /**
+     * Funkcija patikrina ar skaičius yra lyginis
+     * @param x tikrinamas skaičius
+     * @return grąžina true, jei lyginis; grąžina false, jei nelyginis.
+     */
+    public static boolean arSkaiciusLyginisIrNeDvejetas(int x) {
+        return x % 2 == 0 && x > 2;
+    }
+
+
+    /**
+     * Funkcija konvertuoja boolean reikšmę į paprastas angliškas žodines reikšmes (yes ir no)
+     * @param b boolean reikšmė
+     * @return Yes jeigu true, No jeigu false
+     */
+    public static String konvertuojaYesIrNo(boolean b) {
+        if (b) {
+            return "Yes";
+        }
+        else {
+            return "No";
+        }
     }
 }
